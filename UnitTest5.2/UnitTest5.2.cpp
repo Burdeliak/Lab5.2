ed9b6d4c6ce06b3efce9ea69728beae84f06f12f
Олександр <oleksandr140407@gmail.com>
2024-10-09 02:25:12 +1000

    Lab5.2

---
+++ UnitTest5.2/UnitTest5.2.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.2/lab 5.2.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double R = 9.0 / 3;
+			Assert::AreEqual(R, 3.0);
+		}
+	};
+}
